e8822ee63a7f9288ed9d163df98f0512e8902ede
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-06-04 02:17:04 +0200

    implements main navigation

--- lhamidou-nuxt/app.vue
+++ lhamidou-nuxt/app.vue
@@ -11,25 +11,34 @@
 </script>
 
 <template>
-  <div>
-  
-    <nav>
-      <NuxtLink to="/"  class="navbar-brand">l'Hamidou.</NuxtLink>
-      <ul class="justify-content-center isotope-options mb-60 pb-30">
-        <li class="nav-item">
-          <NuxtLink to="/logos"  class="nav-link aos-init aos-animate nav-link-name">logos</NuxtLink>
-        </li>
-        <li class="nav-item">
-          <NuxtLink to="/"  class="nav-link aos-init aos-animate nav-link-name">flyers</NuxtLink>
-        </li>
-      </ul>
-      <div class="container justify-content-between">            
-            <div class="navbar-toggle">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div> 
+  <div>  
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <NuxtLink class="nav-link active" aria-current="page" to="/">Home</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link" to="/logos">Logos</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link" to="/flyers">Flyers</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink class="nav-link" to="/contact">Contact</NuxtLink>
+            </li>
+          </ul>
+          <!-- <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form> -->
+        </div>
+      </div>
     </nav>
   
     <NuxtPage/>
