98319927445aa4bb630720c0a494f3a6ee3045ef
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-06-04 02:27:28 +0200

    create hero component

---
+++ lhamidou-nuxt/components/Hero.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="pt-60 pb-130 mt-30">
+            <div class="gh-1 gv-5 align-items-center justify-content-center text-center text-lg-start row">
+                <div class="col-auto order-1 order-lg-3 col">
+                    <div class="shape-parent lines-style-2">
+                        <div class="line text-dark"></div>
+                        <div class="shape mt-n30 ms-n30 me-100 me-lg-0 d-block">
+                            <svg data-rellax-speed="-1" width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="45" cy="45" r="45" fill="#F5F5F5"></circle>
+                            </svg>
+                        </div>
+                        <img class="position-relative rounded-circle aos-init aos-animate" width="180" src="~/assets/images/avatar-freelancer.png" alt="" data-aos="fade">
+                    </div>
+                </div>                     
+                <div class="col-12 col-lg-2 text-center text-lg-end order-2 order-lg-4 mt-n60 mt-lg-0 col">
+                    <ul class="nav nav-gap-sm d-inline-flex align-items-center flex-lg-column">
+                        <li class="nav-item">
+                            <a data-aos="fade-up" class="nav-link aos-init aos-animate" href="//facebook.com/runwebrun">
+                                <svg width="7" height="15" viewBox="0 0 10 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.17421 3.65234H9.99996V0.154687C9.68557 0.107422 8.60224 0 7.34088 0C4.70831 0 2.90529 1.82188 2.90529 5.16914V8.25H0V12.1602H2.90529V22H6.46588V12.1602H9.25375L9.69693 8.25H6.46588V5.55586C6.46588 4.42578 6.7424 3.65234 8.17421 3.65234Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a data-aos="fade-up" class="nav-link aos-init aos-animate" href="//instagram.com/runwebrun">
+                                <svg width="15" height="15" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.2827 5.3166C8.24087 5.3166 5.78732 7.8148 5.78732 10.912C5.78732 14.0092 8.24087 16.5074 11.2827 16.5074C14.3245 16.5074 16.7781 14.0092 16.7781 10.912C16.7781 7.8148 14.3245 5.3166 11.2827 5.3166ZM11.2827 14.5497C9.31698 14.5497 7.70998 12.9183 7.70998 10.912C7.70998 8.90563 9.3122 7.27425 11.2827 7.27425C13.2532 7.27425 14.8554 8.90563 14.8554 10.912C14.8554 12.9183 13.2484 14.5497 11.2827 14.5497ZM18.2846 5.08772C18.2846 5.81331 17.7107 6.39282 17.0029 6.39282C16.2902 6.39282 15.7211 5.80844 15.7211 5.08772C15.7211 4.36699 16.295 3.78261 17.0029 3.78261C17.7107 3.78261 18.2846 4.36699 18.2846 5.08772ZM21.9243 6.4123C21.843 4.66404 21.4508 3.11545 20.1929 1.83956C18.9399 0.563678 17.419 0.164355 15.7019 0.0766992C13.9323 -0.0255664 8.62827 -0.0255664 6.85865 0.0766992C5.14643 0.159486 3.62552 0.558809 2.36766 1.83469C1.10979 3.11058 0.722392 4.65917 0.636302 6.40743C0.535865 8.20925 0.535865 13.6098 0.636302 15.4117C0.717609 17.1599 1.10979 18.7085 2.36766 19.9844C3.62552 21.2603 5.14165 21.6596 6.85865 21.7473C8.62827 21.8495 13.9323 21.8495 15.7019 21.7473C17.419 21.6645 18.9399 21.2652 20.1929 19.9844C21.446 18.7085 21.8382 17.1599 21.9243 15.4117C22.0247 13.6098 22.0247 8.21412 21.9243 6.4123ZM19.6381 17.345C19.2651 18.2995 18.5429 19.0348 17.6007 19.4195C16.1898 19.9893 12.8419 19.8578 11.2827 19.8578C9.72352 19.8578 6.37081 19.9844 4.96469 19.4195C4.02727 19.0397 3.30507 18.3043 2.92724 17.345C2.36766 15.9084 2.49679 12.4995 2.49679 10.912C2.49679 9.32443 2.37244 5.91071 2.92724 4.47899C3.30029 3.52451 4.02248 2.78917 4.96469 2.40446C6.37559 1.83469 9.72352 1.96618 11.2827 1.96618C12.8419 1.96618 16.1946 1.83956 17.6007 2.40446C18.5381 2.7843 19.2603 3.51964 19.6381 4.47899C20.1977 5.91558 20.0686 9.32443 20.0686 10.912C20.0686 12.4995 20.1977 15.9133 19.6381 17.345Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="freelance-details col-12 col-lg-6 order-3 order-lg-1 col">
+                    <p class="subtitle aos-init aos-animate" data-aos="fade-up">Hamidou SOW</p>
+                    <h1 class="h2 aos-init aos-animate" data-aos="fade-up">I’m freelance product designer based in LA.</h1>
+                    <a data-aos="fade-up" class="btn btn-dark btn-with-ball mt-5 aos-init aos-animate" href="/themebau/react/contact1">let’s work <span class="btn-ball" style="transform: translateY(0px);"></span>
+                    </a>
+                </div>
+            </div> 
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
--- lhamidou-nuxt/pages/index.vue
+++ lhamidou-nuxt/pages/index.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>    
-    index          
+    <Hero />
     
 </template>
 
