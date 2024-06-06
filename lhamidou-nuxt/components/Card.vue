67dd1fb8692b0eca515376509ca65af5d1a472e8
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-06-06 12:53:19 +0200

    update card component; import card component in grid component

--- lhamidou-nuxt/components/Card.vue
+++ lhamidou-nuxt/components/Card.vue
@@ -5,20 +5,19 @@
 </script>
 
 <template>
-    <div data-filters="branding" class="branding col-12 isotope-item col-md-6 pt-100 px-0">
-        <span class="d-block">
+    <span class="d-block">
             <a class="card card-portfolio card-default card-image-md card-hover-zoom"
                 href="/themebau/react/portfolio-single-style-1">
                 <span class="card-img rounded-0" data-cursor-style="cursor-circle" data-cursor-text="view">
-                    <img src="~/assets/images/africarobase-img-1.jpg" alt="" data-aos="fade-up" class="aos-init aos-animate">
+                    <img src="~/assets/images/africarobase-img-1.jpg" alt="" data-aos="fade-up"
+                        class="aos-init aos-animate">
                 </span>
                 <span class="card-body">
                     <span class="card-title h4">{{ props.project.pro_name }}</span>
-                    <span class="card-category subtitle">allbranding</span>
+                    <span class="card-category subtitle" v-for="category in props.project.pro_category">{{ category }}</span>
                 </span>
             </a>
-        </span>
-    </div>
+    </span>
 </template>
 
 
--- lhamidou-nuxt/components/GridCards.vue
+++ lhamidou-nuxt/components/GridCards.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import Card from './Card.vue';
+
 
 const { data } = await useFetch('/api/hello')
 
@@ -8,19 +10,7 @@ const { data } = await useFetch('/api/hello')
 <template>
     <div class="container pb-160">
         <div data-filters="branding" class="branding col-12 isotope-item col-md-6 pt-100 px-0" v-for="project in data">
-            <span class="d-block">
-                <a class="card card-portfolio card-default card-image-md card-hover-zoom"
-                    href="/themebau/react/portfolio-single-style-1">
-                    <span class="card-img rounded-0" data-cursor-style="cursor-circle" data-cursor-text="view">
-                        <img src="~/assets/images/africarobase-img-1.jpg" alt="" data-aos="fade-up"
-                            class="aos-init aos-animate">
-                    </span>
-                    <span class="card-body">
-                        <span class="card-title h4">{{ project.pro_name }}</span>
-                        <span class="card-category subtitle" v-for="category in project.pro_category">{{ category }}</span>
-                    </span>
-                </a>
-            </span>
+            <Card v-bind:project="project" :key="project.pro_id"/>
         </div>
     </div>
 </template>
