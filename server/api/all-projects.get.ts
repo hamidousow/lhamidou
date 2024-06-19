ae9c2081c0d9ec45fd5ffd8f380b4787d398be24
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-06-20 01:20:08 +0200

    host

--- server/api/all-projects.get.ts
+++ server/api/all-projects.get.ts
@@ -2,7 +2,10 @@ import "dotenv/config"
 
 export default defineEventHandler(async () => {
 
-    const response = await fetch('http://localhost:8888/.netlify/functions/all-projects');
+    const localHost = "http://localhost:8888"
+    const appHost = "https://lhamidou.netlify.app"
+
+    const response = await fetch(`${appHost}/.netlify/functions/all-projects`);
     
     console.log(response);       
     return response;
