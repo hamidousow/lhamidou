3bc5b9f71038b27d92cccc400b78e2fe5041b0dc
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-06-21 00:59:34 +0200

    update nav menu

--- components/Nav.vue
+++ components/Nav.vue
@@ -11,8 +11,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
                         <NuxtLink class="nav-link active" aria-current="page" to="/">Home</NuxtLink>
                     </li>                    
