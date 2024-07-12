ea386a446ff11d1f82bbd93b15c53ae8995d8b63
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-07-12 16:58:47 +0200

    implements responsive to  navigation's menu

--- components/NavComponent.vue
+++ components/NavComponent.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 
-    const show = ref(false)
+    const isActive = ref(false)
+    const activeMenu = ref('active')
 
 
 </script>
@@ -9,39 +10,42 @@
     <nav class="navbar position-fixed start-0 end-0 bg-body-tertiary">
         <div class="container">
             <NuxtLink class="navbar-brand climate-crisis-navbar-brand" to="/">L'Hamidou</NuxtLink>
-            <div class="nav-menu-icon" @click="show = !show">
+            <div class="nav-menu-icon" @click="isActive = !isActive">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
                     <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
                 </svg>
             </div>
-            <Transition name="fade">
-                <div v-if="show" class="nav-menu menu-open background-black">
-                    <div class="nav-menu-close position-absolute top-0 end-0 mt-3 me-3" data-bs-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation" @click="show = !show">
-                        <span class="nav-menu-close-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
-                                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-                            </svg>
-                        </span>
-                    </div>
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <NuxtLink 
-                                class="nav-link climate-crisis-navbar-menu color-green-flash" 
-                                aria-current="page" 
-                                to="/"  
-                                @click="show = !show"
-                            >Home</NuxtLink>
-                        </li>                    
-                        <li class="nav-item">
-                            <NuxtLink 
-                                class="nav-link climate-crisis-navbar-menu color-green-flash" 
-                                to="/contact"
-                                @click="show = !show"                            
-                            >Contact</NuxtLink>
-                        </li>
-                    </ul>
+            
+            <div                     
+                class="nav-menu"
+                :class="[isActive ? activeMenu : '']"
+            >
+                <div class="nav-menu-close position-absolute top-0 end-0 mt-3 me-3"  @click="isActive = !isActive">
+                    <span class="nav-menu-close-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                        </svg>
+                    </span>
                 </div>
-            </Transition>
+                <ul class="menu my-0">
+                    <li class="menu-item">
+                        <NuxtLink 
+                            class="menu-item-link" 
+                            aria-current="page" 
+                            to="/"  
+                            @click="isActive = !isActive"
+                        >Home</NuxtLink>
+                    </li>                    
+                    <li class="menu-item">
+                        <NuxtLink 
+                            class="menu-item-link" 
+                            to="/contact"
+                            @click="isActive = !isActive"                          
+                        >Contact</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+            
         </div>
     </nav>
 </template>
@@ -51,29 +55,6 @@
 <style>
     .navbar {
         z-index: 99;
-    }
-
-    .nav-menu {
-        top: 0;
-        left: 0;            
-    }
-
-    .menu-open {
-        position: fixed;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100dvh;
-        width: 100%;
-        z-index: 999;
-        transition: .9s;
-    }
-    .menu-open a {
-        text-align: center;
-    }
-
-    .nav-item a:hover {
-        color: var(--clr-grey-light);
     }
 
     .navbar-brand {
@@ -88,19 +69,42 @@
         white-space: nowrap;
     }
 
+    .active-menu {
+        display: flex;
+    }
+
+    .nav-menu {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;   
+        justify-content: center;
+        align-items: center;
+        height: 100dvh;
+        width: 100%;
+        z-index: 999;
+        transition: .9s;   
+        background: var(--clr-black);    
+    }
+    .nav-menu.active {
+        display: flex !important;
+    }
+    .menu-item-link {
+        text-align: center;
+        font-family: 'Climate Crisis', sans-serif;
+        font-optical-sizing: auto;
+        font-size: 38px;
+        font-weight: 400;
+        font-style: normal;
+        font-variation-settings: "YEAR" 1979;
+
+        color: var(--clr-green-flash);
+    }
+
 
     .climate-crisis-navbar-brand {
         font-family: "Climate Crisis", sans-serif;
         font-optical-sizing: auto;
-        font-weight: 400;
-        font-style: normal;
-        font-variation-settings: "YEAR" 1979;
-    }
-
-    .climate-crisis-navbar-menu {
-        font-family: "Climate Crisis", sans-serif;
-        font-optical-sizing: auto;
-        font-size: 38px;
         font-weight: 400;
         font-style: normal;
         font-variation-settings: "YEAR" 1979;
@@ -116,6 +120,50 @@
     .fade-enter-from,
     .fade-leave-to {
         opacity: 0;
+    }
+
+    @media screen and (min-width: 768px) {
+
+        .nav-menu-icon {
+            display: none;
+            visibility: hidden;
+        }
+
+        .nav-menu-close {
+            display: none;
+        }
+
+        .nav-menu {
+            display: flex!important;
+            position: relative;
+            justify-content: flex-end;
+            width: auto;
+            height: auto;
+            background-color: transparent;
+        }
+        .menu {
+            display: flex;
+            flex-direction: row;
+            column-gap: 16px;
+        }
+        .menu li {
+            list-style-type: none;
+        }
+        
+        .menu-item-link {
+            font-size: 16px;
+            color: var(--clr-black);        
+            text-align: center;
+            font-size: 16px;
+            font-style: normal;
+            font-variation-settings: "YEAR" 1979;
+            transition: color .7s;
+
+            &:hover {
+                
+                color: var(--clr-purple);
+            }
+        }
     }
 
 
--- pages/contact.vue
+++ pages/contact.vue
@@ -17,8 +17,8 @@
       <div class="mb-n8 row">
         <div data-aos="fade-up" class="col-12 col-lg-10 col-xl-8 aos-init aos-animate">
           <h1 class="h2 mt-15 pb-4 pb-sm-0">Besoin d'un <strong> logo </strong> unique et personnalisé ? D'un <strong> site vitrine </strong> fait sur mesure et SEO friendly ? </h1>
-          <p> Envoyez-moi un message et démarrer votre projet dès aujourd'hui ! </p>
-          <ButtonComponent :to="`mailto:${decodedEmail}`"> Passer à l'action 🚀 </ButtonComponent>
+          <p> Envoyez-moi un message pour démarrer votre projet dès aujourd'hui ! </p>
+          <ButtonComponent :to="`mailto:${decodedEmail}`"> Passez à l'action 🚀 </ButtonComponent>
         </div>
       </div>
     </div>    
